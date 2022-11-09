62f40986e8c52133162efeef524316ee67bcf768
MellyMel-appdev4ever <117511966+braja91221@users.noreply.github.com>
2022-11-09 15:18:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,19 +51,30 @@ except URLError as e:
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 #streamlit.dataframe(fruityvice_normalized)
-streamlit.stop()
+
 
 #import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+#snowflake-related functions
+def get_fruit_load_list()
+    with my_cnx_.cursor() as my_cur
+    #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_cur = my_cnx.cursor()
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_cur.fetchall()
+ #my_data_rows = my_cur.fetchall()
 #my_data_row = my_cur.fetchone()
 #streamlit.text("The fruit load list contains:")
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+
+#add a button to load the fruit
+if streamlit.button('Get fruit load list')
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows=get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
 #streamlit.dataframe(my_data_row)
 #streamlit.text(my_data_row)
+streamlit.stop()
+
 add_my_fruit = streamlit.text_input('What fruit would you like add')
 streamlit.write('Thanks for adding ', add_my_fruit)
 my_cur.execute("insert into fruit_load_list values('from streamlit')")
